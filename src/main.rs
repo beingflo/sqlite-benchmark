b3bc95e89665f088affd8bf7ec33af60d3d31635
Florian Marending <florian@marending.dev>
2023-12-29 20:33:52 +0100

    Add index read

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
 use crate::scenarios::{
-    index::index, simple::simple, simple_read::simple_read, single_mutex::single_mutex, wal::wal,
-    wal_read::wal_read, wal_synchronous::wal_synchronous,
+    index::index, index_read::index_read, simple::simple, simple_read::simple_read,
+    single_mutex::single_mutex, wal::wal, wal_read::wal_read, wal_synchronous::wal_synchronous,
     wal_synchronous_memory::wal_synchronous_memory,
     wal_synchronous_memory_read::wal_synchronous_memory_read,
     wal_synchronous_read::wal_synchronous_read,
@@ -16,6 +16,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Single Thread
     print!("Single thread + Index: ");
     index()?;
+    print!("Single thread + Index read: ");
+    index_read()?;
+    fs::remove_file("./index.sqlite")?;
 
     print!("Single thread + WAL mode + synchronous normal + in-memory: ");
     let conn = wal_synchronous_memory()?;
